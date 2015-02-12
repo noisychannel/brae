Added semantic tuning (not enabled). LBFGS and SGD optimization for backpropagation. There may be a bug in the gradient calculation which causes the cost to increase every iteration.

src/autoencoder.py
@@ -3,39 +3,27 @@ import activationfunction as activationfunction
 
 class AutoEncoder:
 
-  # Dimensionality of input
-  n = 100
-
-  # The parameter matrix W_1 (n X 2n)
-  paramMatrix = None
-
-  # The bias vector b_1 (n X 1)
-  bias = None
-
-  # The reconstruction parameter matrix W_2 (2n X n)
-  reconParamMatrix = None
-
-  # The bias vector b_2 (2n X 1)
-  reconBias = None
-
-  # The activation function
-  activation = None
-
   def __init__(self, n=100, activation=None):
     self.n = n
     if activation:
       self.activation = activation
     else:
       self.activation = activationfunction.ActivationFunction("tanh")
-    self.paramMatrix = np.random.rand(n, 2*n)
-    self.bias = np.random.rand(n, 1)
-    self.reconParamMatrix = np.random.rand(2*n, n)
-    self.reconBias = np.random.rand(2*n, 1)
+    if self.activation.funcType == "tanh":
+      # Initialization of weight matrix based on Xavier_10
+      self.paramMatrix = np.random.uniform(-np.sqrt(6./(n + 2*n)), np.sqrt(6./(n + 2*n)), (n, 2*n))
+      self.reconParamMatrix = np.random.uniform(-np.sqrt(6./(n + 2*n)), np.sqrt(6./(n + 2*n)), (2*n, n))
+    elif self.activation.funcType == "sigmoid":
+      # Initialization of weight matrix based on Xavier_10
+      self.paramMatrix = np.random.uniform(-4. * np.sqrt(6./(n + 2*n)), 4. * np.sqrt(6./(n + 2*n)), (n, 2*n))
+      self.reconParamMatrix = np.random.uniform(-4. * np.sqrt(6./(n + 2*n)), 4. * np.sqrt(6./(n + 2*n)), (2*n, n))
+    self.bias = np.zeros((n, 1))
+    self.reconBias = np.zeros((2*n, 1))
 
   # Computes the parent vector from the input vectors
   # c1 and c2 are n-dimensional vectors
   # p = f(W * [c1;c2] + b)
-  def computeParentVector(self, c1, c2):
+  def computeParentVector(self, c1, c2, paramMatrix, bias):
     # Sanity check for dimensionality of input
     try:
       (np.shape(c1) == (self.n, 1)) & (np.shape(c2) == (self.n, 1))
@@ -48,7 +36,7 @@ class AutoEncoder:
 
     c1c2 = np.vstack((c1, c2))
     # Parent before the activation function is applied
-    p_z = np.dot(self.paramMatrix, c1c2) + self.bias
+    p_z = np.dot(paramMatrix, c1c2) + bias
     # Application of the activation function
     p = self.activation.apply(p_z)
 
@@ -58,7 +46,7 @@ class AutoEncoder:
   # for one training example
   # No regularization included here. It can be included when calculating the total error
   # [c1':c2'] = f(W_2 * p + b_2)
-  def computeSingleReconstructionError(self, p, c1, c2):
+  def computeSingleReconstructionError(self, p, c1, c2, reconParamMatrix, reconBias):
     try:
       (np.shape(c1) == (self.n, 1)) & (np.shape(c2) == (self.n, 1)) & (np.shape(p) == (self.n, 1))
     except:
@@ -69,7 +57,7 @@ class AutoEncoder:
       print "Got : p = " + str(np.shape(c2))
       raise
 
-    c1c2Prime_z = np.dot(self.reconParamMatrix, p) + self.reconBias
+    c1c2Prime_z = np.dot(reconParamMatrix, p) + reconBias
     c1c2Prime = self.activation.apply(c1c2Prime_z)
     reconstructionError = self.computeL2Norm(c1c2Prime, np.vstack((c1,c2)))
 
@@ -89,13 +77,18 @@ class AutoEncoder:
 
     return np.sqrt(np.sum(inputDiff**2))
 
-  def getGradients(self, training, regularization=True, l=0.1):
+  def getGradients(self, training, paramMatrix, reconParamMatrix, regularization=True, l=0.1, alpha=0.1, delta_3_s=None, W_s=None):
+    """
+    If alpha and delta_3_s are set, then we are trying to calculate semantic gradients
+    That is, the size of W(2) now becomes, (3n X n) and the size of b(3) becomes (3n x 1)
+    delta_3 = [delta_3_r, delta_3_s] -> 3n X 1
+    """
     # Initialize weight and bias accumulators
     weightAccumulator_1 = np.zeros((self.n, self.n*2))
     weightAccumulator_2 = np.zeros((self.n*2, self.n))
     biasAccumulator_2 = np.zeros((self.n, 1))
     biasAccumulator_3 = np.zeros((self.n * 2, 1))
-    for instance in training:
+    for i, instance in enumerate(training):
       # First compute forward activations for each layer for this training instance
       # 2n X 1
       x = instance[0]
@@ -106,14 +99,31 @@ class AutoEncoder:
       activationValues_3 = instance[3]
       z_3 = instance[4]
 
+      # Skip training examples that are None. 
+      if x is None:
+        continue
+
       # Now calculate the delta terms for each layer and add to the accumulator
       # 2n X 1
       delta_3 = self.computeL2Norm(activationValues_3, x) * \
           self.activation.derivative(z_3)
-      # Note the use of the Hadamard product here
-      # n x 1
-      delta_2 = np.dot(np.transpose(self.reconParamMatrix), delta_3) * \
-          self.activation.derivative(z_2)
+      W_2 = reconParamMatrix
+
+      if (delta_3_s is not None):
+        # Semantic tuning
+        if (delta_3_s[i] is None):
+          continue
+        # Now 3n X 1
+        delta_3_tmp = np.vstack((alpha * delta_3, (1 - alpha) * delta_3_s[i]))
+        # Now 3n X n
+        W_2_tmp = np.vstack((W_2, W_s))
+        delta_2 = np.dot(np.transpose(W_2_tmp), delta_3_tmp) * \
+            self.activation.derivative(z_2)
+      else:
+        # Note the use of the Hadamard product here
+        # n x 1
+        delta_2 = np.dot(np.transpose(W_2), delta_3) * \
+            self.activation.derivative(z_2)
 
       # Now add these layer errors to the accumulator
       weightAccumulator_1 = weightAccumulator_1 + np.dot(delta_2, np.transpose(x))
@@ -121,14 +131,18 @@ class AutoEncoder:
       weightAccumulator_2 = weightAccumulator_2 + np.dot(delta_3, np.transpose(activationValues_2))
       biasAccumulator_3 = biasAccumulator_3 + delta_3
 
+#      print np.sqrt(np.sum(activationValues_3**2))
+      #print np.sqrt(np.sum(x**2))
+
     # Compute derivatives now
     m = len(training)
     dJ_dW1 = (1./m) * weightAccumulator_1
     dJ_dW2 = (1./m) * weightAccumulator_2
     if (regularization):
-      dJ_dW1 = dJ_dW1 + l * self.paramMatrix
-      dJ_dW2 = dJ_dW2 + l * self.reconParamMatrix
+      dJ_dW1 = dJ_dW1 + l * paramMatrix
+      dJ_dW2 = dJ_dW2 + l * reconParamMatrix
     dJ_db2 = (1./m) * biasAccumulator_2
     dJ_db3 = (1./m) * biasAccumulator_3
 
     return dJ_dW1, dJ_dW2, dJ_db2, dJ_db3
+

src/phraseEmbedding.py
@@ -2,19 +2,11 @@
 
 import numpy as np
 import autoencoder as autoencoder
+from scipy.optimize import minimize
+import sys
+import copy
 
 class PhraseEmbeddingModel:
-  # Dimensionality of the input vector
-  n = 50
-  # A Word vector object that holds pre-trained word embeddings for the vocabulary
-  w = None
-  # Training data for backprop
-  backPropTrainingData = []
-  # Captures total reconstruction error per iteration of backprop
-  totalReconstructionError = 0
-  # An autoencoder that is shared by this phrase embedding model (RAE)
-  a = None
-
 
   def __init__(self, n, w):
     """
@@ -30,9 +22,19 @@ class PhraseEmbeddingModel:
     self.w = w
     self.a = autoencoder.AutoEncoder(self.n)
 
+    #self.a.paramMatrix = np.array([[-0.66901207, -0.18282628, -0.05600838, -0.93896959],
+                                  #[-0.10363659, -0.53908909, 0.24603079, 0.50378757]])
+    #self.a.reconParamMatrix = np.array([[-0.22829916, 0.92183385],
+                                        #[-0.4777748, 0.47174915],
+                                        #[ 0.22207423, -0.22005313],
+                                        #[-0.53727245, -0.70092649]])
+    self.totalReconstructionError = 0
+    self.backPropTrainingData = []
+    self.finalLayerBackPropData = []
+
 
   # Given a source string 
-  def findBestNodesToCombine(self, phraseVectors):
+  def findBestNodesToCombine(self, phraseVectors, paramMatrix, bias, reconParamMatrix, reconBias):
     """
     Given a phrase and the embeddings of each word in the phrase, this
     finds the best leaves to combine based on a greedy metric
@@ -57,8 +59,8 @@ class PhraseEmbeddingModel:
     errors = []
     for i in range(len(phraseVectors) - 1):
       # Check reconstruction error for (i, i+1)
-      p_z, p = self.a.computeParentVector(phraseVectors[i], phraseVectors[i+1])
-      c1c2Prime_z, c1c2Prime, reconError = self.a.computeSingleReconstructionError(p, phraseVectors[i], phraseVectors[i+1])
+      p_z, p = self.a.computeParentVector(phraseVectors[i], phraseVectors[i+1], paramMatrix, bias)
+      c1c2Prime_z, c1c2Prime, reconError = self.a.computeSingleReconstructionError(p, phraseVectors[i], phraseVectors[i+1], reconParamMatrix, reconBias)
       if reconError < minError:
         minError = reconError
         minErrorIndex = i
@@ -73,7 +75,7 @@ class PhraseEmbeddingModel:
     return minErrorIndex, minError, minErrorParent, minErrorParent_z, minErrorC1C2Prime, minErrorC1C2Prime_z
 
 
-  def computeOptimalBinaryTree(self, phrase):
+  def computeOptimalBinaryTree(self, phrase, paramMatrix, bias, reconParamMatrix, reconBias):
     """
     Given a phrase and the embeddings of each word in the phrase, this constructs
     an 'optimal' binary tree for phrase embedding. This is based on the greedy
@@ -82,11 +84,17 @@ class PhraseEmbeddingModel:
 
     Parameters:
     phrase : A phrase for which a binary tree for phrase embedding needs to be created
+
+    Returns
+      The result of the phrase embedding. This is the final parent with the same dimensionality
+      as the word vectors
     """
 
+    #TODO: Why is this done everytime? the wrapper should send back phrase vectors
     words = phrase.split()
     words = [word for word in words if self.w[word] != None]
     phraseVectors = []
+    treeConstructed = False
 
     for word in words:
       phraseVectors.append(np.reshape(self.w[word], (self.n,1)))
@@ -97,20 +105,71 @@ class PhraseEmbeddingModel:
     while len(phraseVectors) > 1:
       # A few values (while creation of the best tree) are cached for use in backprop
       combinedIndex, combinedError, combinedParent, combinedParent_z, \
-          reconstructedC1C2, reconstructedC1C2_z = self.findBestNodesToCombine(phraseVectors)
+          reconstructedC1C2, reconstructedC1C2_z = self.findBestNodesToCombine(phraseVectors, paramMatrix, bias, reconParamMatrix, reconBias)
       # Cache values for backprop training and calculating gradients
       self.backPropTrainingData.append((np.vstack((phraseVectors[combinedIndex], phraseVectors[combinedIndex+1])), \
           combinedParent, combinedParent_z, reconstructedC1C2, reconstructedC1C2_z))
+###########
+      #if len(phraseVectors) == 2:
+        ## Keep track of this for semantic tuning
+        #self.finalLayerBackPropData.append((np.vstack((phraseVectors[combinedIndex], phraseVectors[combinedIndex+1])), \
+          #combinedParent, combinedParent_z, reconstructedC1C2, reconstructedC1C2_z))
+###########
+
       # Record total reconstruction error
-      self.totalReconstructionError = self.totalReconstructionError + combinedError**2
+      self.totalReconstructionError = self.totalReconstructionError + 0.5 * combinedError**2
       # Change the phrase vector to include the parent
       phraseVectors[combinedIndex] = combinedParent
       del phraseVectors[combinedIndex+1]
       combinationPattern[combinedIndex] = (combinationPattern[combinedIndex], combinationPattern[combinedIndex+1])
       del combinationPattern[combinedIndex+1]
+    #print combinationPattern
+##########
+      #treeConstructed = True
 
+    #if len(phraseVectors) > 1:
+      #raise Exception("Phrase vector contains multiple elements. Tree building did not succeed")
+    #else:
+      #if treeConstructed:
+        ## The opposite case is when we encouter a phrase that only contained OOVs wrt the 
+        ## word embeddings or we encounter a single word phrase
+        #return phraseVectors[0]
+      #else:
+        #self.finalLayerBackPropData.append((None, None, None, None, None))
+        #return None
+##########
 
   def train(self, phrases, numIterations, regularLambda, learningRate):
+    self.phrases = phrases
+    self.regularLambda = regularLambda
+    self.SGD(numIterations, learningRate)
+    #self.LBFGS(numIterations)
+
+
+  def LBFGS(self, numIterations):
+    initWeights = self.foldWeights(self.a.paramMatrix, self.a.reconParamMatrix, self.a.bias, self.a.reconBias)
+    # Get gradients once to fill in the backprop data which is required to calculate cost
+    self.getGradients(initWeights)
+    res = minimize(self.computeCost, initWeights, method='L-BFGS-B', jac=self.getGradients, tol=0.00001, options={'disp': True, 'maxiter':numIterations})
+    print res.message
+
+
+  def SGD(self, numIterations, learningRate):
+    for i in range(numIterations):
+      # Minimize
+      # SGD : Update parameters
+      weights = self.foldWeights(self.a.paramMatrix, self.a.reconParamMatrix, self.a.bias, self.a.reconBias)
+      dJ_dW1, dJ_dW2, dJ_db2, dJ_db3 = self.unfoldWeights(self.getGradients(weights))
+      self.gradient_checking(weights, 1e-15)
+
+      self.computeCost(weights)
+      self.a.paramMatrix = self.a.paramMatrix - learningRate * dJ_dW1
+      self.a.reconParamMatrix = self.a.reconParamMatrix - learningRate * dJ_dW2
+      self.a.bias = self.a.bias - learningRate * dJ_db2
+      self.a.reconBias = self.a.reconBias - learningRate * dJ_db3
+
+
+  def getGradients(self, weights):
     """
     Trains this phrase embedding model with backpropagation
 
@@ -121,23 +180,89 @@ class PhraseEmbeddingModel:
     learningRate : The learning rate for SGD
     """
 
-    for i in range(numIterations):
-      for phrase in phrases:
-        self.computeOptimalBinaryTree(phrase)
+    # Reset reconstruction error for the next iteration
+    self.totalReconstructionError = 0.
+    self.backPropTrainingData = []
 
-      cost = (1./len(self.backPropTrainingData)) * self.totalReconstructionError + \
-          regularLambda * (np.sum(self.a.paramMatrix * self.a.paramMatrix) + \
-          np.sum(self.a.reconParamMatrix * self.a.reconParamMatrix))
-      print "Cost (" + str(i) + ") = " + str(cost)
+    paramMatrix, reconParamMatrix, bias, reconBias = self.unfoldWeights(weights)
 
-      # Get partial derivatives
-      dJ_dW1, dJ_dW2, dJ_db2, dJ_db3 =  self.a.getGradients(self.backPropTrainingData, True, regularLambda)
+    for phrase in self.phrases:
+      #self.computeOptimalBinaryTree(phrase, self.a.paramMatrix, self.a.bias, self.a.reconParamMatrix, self.a.reconBias)
+      self.computeOptimalBinaryTree(phrase, paramMatrix, bias, reconParamMatrix, reconBias)
 
+    # Get partial derivatives
+    # TODO: Enable regularization later
+    # TODO: When regularization is enabled, send bias and reconBias as well
+    dJ_dW1, dJ_dW2, dJ_db2, dJ_db3 =  self.a.getGradients(self.backPropTrainingData, paramMatrix, reconParamMatrix, False, self.regularLambda)
+
+############
+      #self.finalLayerBackPropData = []
+    return self.foldWeights(dJ_dW1, dJ_dW2, dJ_db2, dJ_db3)
+
+
+  def computeCost(self, weights):
+    #TODO: Replace withj regularized cost
+    #cost = (1./len(self.backPropTrainingData)) * self.totalReconstructionError + \
+        #self.regularLambda * (np.sum(self.a.paramMatrix ** 2) + np.sum(self.a.reconParamMatrix ** 2 ))
+    cost = (1./len(self.backPropTrainingData)) * self.totalReconstructionError
+    print "Cost = " + str(cost)
+    return cost
+
+
+  def foldWeights(self, paramMatrix, reconParamMatrix, bias, reconBias):
       # SGD : Update parameters
-      self.a.paramMatrix = self.a.paramMatrix - learningRate * dJ_dW1
-      self.a.reconParamMatrix = self.a.reconParamMatrix - learningRate * dJ_dW2
-      self.a.bias = self.a.bias - learningRate * dJ_db2
-      self.a.reconBias = self.a.reconBias - learningRate * dJ_db3
+    weights = np.reshape(paramMatrix, (1, np.shape(paramMatrix)[0] * np.shape(paramMatrix)[1]))[0]
+    weights = np.hstack((weights, np.reshape(reconParamMatrix, (1, np.shape(reconParamMatrix)[0] * np.shape(reconParamMatrix)[1]))[0]))
+    weights = np.hstack((weights, np.reshape(bias, (1, np.shape(bias)[0]))[0]))
+    weights = np.hstack((weights, np.reshape(reconBias, (1, np.shape(reconBias)[0]))[0]))
+    return weights
 
-      # Reset reconstruction error for the next iteration
+
+  def unfoldWeights(self, weights):
+    # TODO: Too many redundant actions, re-write
+    paramMatrixShape = np.shape(self.a.paramMatrix)
+    paramMatrixSize = paramMatrixShape[0] * paramMatrixShape[1]
+    tmp = weights[0:paramMatrixSize]
+    tmpRemaining = weights[paramMatrixSize:]
+    paramMatrix = np.reshape(tmp, paramMatrixShape)
+
+    reconParamMatrixShape = np.shape(self.a.reconParamMatrix)
+    reconParamMatrixSize = reconParamMatrixShape[0] * reconParamMatrixShape[1]
+    tmp = tmpRemaining[0:reconParamMatrixSize]
+    tmpRemaining = tmpRemaining[reconParamMatrixSize:]
+    reconParamMatrix = np.reshape(tmp, reconParamMatrixShape)
+
+    biasShape = np.shape(self.a.bias)
+    biasSize = biasShape[0]
+    tmp = tmpRemaining[0:biasSize]
+    tmpRemaining = tmpRemaining[biasSize:]
+    bias = np.reshape(tmp, biasShape)
+
+    reconBias = np.reshape(tmpRemaining, np.shape(self.a.reconBias))
+
+    return paramMatrix, reconParamMatrix, bias, reconBias
+
+  def gradient_checking(self, theta, eps):
+    f_approx = np.zeros(np.shape(theta))
+    for i, t in enumerate(theta):
+      theta_plus = copy.deepcopy(theta)
+      theta_minus = copy.deepcopy(theta)
+      theta_plus[i] = theta[i] + eps
+      theta_minus[i] = theta[i] - eps
+
+      self.backPropTrainingData = []
       self.totalReconstructionError = 0.
+      for phrase in self.phrases:
+        #self.computeOptimalBinaryTree(phrase, self.a.paramMatrix, self.a.bias, self.a.reconParamMatrix, self.a.reconBias)
+        self.computeOptimalBinaryTree(phrase, paramMatrix, bias, reconParamMatrix, reconBias)
+      cost_theta_plus = self.computeCost(theta_plus)
+
+      self.backPropTrainingData = []
+      self.totalReconstructionError = 0.
+      for phrase in self.phrases:
+        #self.computeOptimalBinaryTree(phrase, self.a.paramMatrix, self.a.bias, self.a.reconParamMatrix, self.a.reconBias)
+        self.computeOptimalBinaryTree(phrase, paramMatrix, bias, reconParamMatrix, reconBias)
+      cost_theta_minus = self.computeCost(theta_minus)
+
+      f_approx[i] = (cost_function(theta_plus, data) - cost_function(theta_minus, data)) / (2 * eps)
+    return f_approx